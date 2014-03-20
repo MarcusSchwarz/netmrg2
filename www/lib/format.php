8ee67e878dc745c41deaf9111893bebb79ee7a3e
Marcus Schwarz <msspamfang@gmx.de>
2014-03-20 21:41:25 +0100

    html5ified

--- www/lib/format.php
+++ www/lib/format.php
@@ -109,15 +109,13 @@ function end_page() {
  * @internal param string $pagename page that this is
  */
 function DisplayPageHeader($prettyname = "", $refresh = false, $display_menu = false, $bodytags = "", $javascript_files = array()) {
-    echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
     ?>
-    <!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <!DOCTYPE html>
 
-    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <html>
 
     <head>
+        <meta charset="utf-8">
         <title><?php print GetPageTitle($prettyname); ?></title>
 
         <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS["netmrg"]["webroot"]; ?>/include/netmrg.css"/>
