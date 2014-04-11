658d2bf656f23ed436831fc63b1f27dc896c2d16
Marcus Schwarz <msspamfang@gmx.de>
2014-04-12 00:15:53 +0200

    added a license header

--- www/lib/DB.php
+++ www/lib/DB.php
@@ -1,11 +1,27 @@
 <?php
 /**
- * (c) Marcus Schwarz
- * Date: 15.03.14
- * Time: 14:45
- * Id:   $Id$
+ * DB.php
  *
- * Powered by Alpen Yod'l Koffein
+ * Functions used for database access of NetMRG
+ * Copyright (c) 2014
+ *   Marcus Schwarz <msspamfang@gmx.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *
+ * @author Marcus Schwarz <msspamfang@gmx.de>
  */
 
 
