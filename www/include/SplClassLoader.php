adfeab0bc27a486c66ef75d09577dbab411f3840
Marcus Schwarz <msspamfang@gmx.de>
2014-04-18 00:34:35 +0200

    if a class file is not found the autoloader should throw an exception

--- www/include/SplClassLoader.php
+++ www/include/SplClassLoader.php
@@ -151,7 +151,13 @@ class SplClassLoader
             }
             $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . $this->_fileExtension;
 
-            require ($this->_includePath !== null ? $this->_includePath . DIRECTORY_SEPARATOR : '') . $fileName;
+            $fileWithPath = ($this->_includePath !== null ? $this->_includePath . DIRECTORY_SEPARATOR : '') . $fileName;
+
+            if (!is_readable($fileWithPath)) {
+                throw new Exception('class not found');
+            }
+
+            require $fileWithPath;
         }
     }
 }
