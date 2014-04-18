6d9f3f285d822a5ac2c570952661551bc5c20305
Marcus Schwarz <msspamfang@gmx.de>
2014-04-18 20:19:49 +0200

    adjusted links and removed about.php

--- www/lib/format.php
+++ www/lib/format.php
@@ -87,7 +87,7 @@ function end_page() {
             <img src="<?php echo get_image_by_name("logo"); ?>" alt="logo"/>
             NetMRG
         </a>
-        <a href="about.php">&copy; 2003-2007</a>
+        <a href="index/about">&copy; 2003-2007</a>
     </div> <!-- end #footer -->
 
     </body>
